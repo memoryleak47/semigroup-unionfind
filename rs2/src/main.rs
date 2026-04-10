390375aadcd3d55340a4f18924748bdc6dece952
memoryleak47 <memoryleak47@protonmail.ch>
2026-04-10 13:06:10 +0200

    ...

--- rs2/src/main.rs
+++ rs2/src/main.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-// TODO do we want interned structured eids, or nah?
-
 mod slotted;
 
 type Id = usize;
@@ -18,6 +16,7 @@ trait Group {
 trait Analysis<L: Language> {
     type G: Group;
 
+    fn canon(n: &L) -> (Self::G, L);
     fn mv(_: Self, _: Self::G) -> Self;
     fn mk(_: L) -> Self;
     fn merge(_: Self, _: Self) -> Self;
@@ -25,14 +24,22 @@ trait Analysis<L: Language> {
 
 struct EGraph<L: Language, N: Analysis<L>> {
     hashcons: HashMap<L, (N::G, Id)>,
+
+    g_hashcons: HashMap<(N::G, Id), Id>,
+    uf: HashMap<Id, (N::G, Id)>
 }
 
 impl<L: Language, N: Analysis<L>> EGraph<L, N> {
     pub fn new() -> Self {
-        EGraph { hashcons: Default::default() }
+        EGraph {
+            hashcons: Default::default(),
+            g_hashcons: Default::default(),
+            uf: Default::default(),
+        }
     }
 
-    pub fn add(&mut self, n: L) -> Id {
+    pub fn add(&mut self, n: &L) -> Id {
+        let (g, n) = N::canon(n);
         todo!()
     }
 }
