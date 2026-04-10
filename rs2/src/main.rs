9c943ab806b936e3d6f6d3442f266b71e90d3f87
memoryleak47 <memoryleak47@protonmail.ch>
2026-04-10 17:02:25 +0200

    ~add/union

--- rs2/src/main.rs
+++ rs2/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 
 mod uf;
 pub use uf::*;
@@ -6,7 +7,7 @@ pub use uf::*;
 mod slotted;
 pub use slotted::*;
 
-trait Language {
+trait Language: Eq + Hash {
 }
 
 trait Analysis<L: Language> {
@@ -14,7 +15,7 @@ trait Analysis<L: Language> {
     type S: Semilattice<G=Self::G>;
 
     fn canon(n: &L) -> (Self::G, L);
-    fn mk(_: L) -> Self::S;
+    fn mk(n: &L) -> Self::S;
 }
 
 struct EGraph<L: Language, N: Analysis<L>> {
@@ -30,9 +31,24 @@ impl<L: Language, N: Analysis<L>> EGraph<L, N> {
         }
     }
 
-    pub fn add(&mut self, n: &L) -> Id {
-        let (g, n) = N::canon(n);
-        todo!()
+    pub fn add(&mut self, n: &L) -> (N::G, Id) {
+        let (g, n2) = N::canon(n);
+        if let Some((g2, x)) = self.hashcons.get(&n2) {
+            // n == g*n2
+            // n2 == g2*x
+            // -> n == g*g2*x
+            (N::G::compose(&g, &g2), *x)
+        } else {
+            let s = N::mk(&n2);
+            let x = self.uf.makeset(s);
+            self.hashcons.insert(n2, (N::G::identity(), x));
+            (g, x)
+        }
+    }
+
+    pub fn union(&mut self, x: (N::G, Id), y: (N::G, Id)) {
+        self.uf.union(x, y)
+        // TODO rebuild
     }
 }
 
