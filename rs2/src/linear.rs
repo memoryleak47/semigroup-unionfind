f5ef62bf8271b733abaa1c89b87684af150a4530
memoryleak47 <memoryleak47@protonmail.ch>
2026-04-23 15:09:45 +0200

    impl linear semilattice

--- rs2/src/linear.rs
+++ rs2/src/linear.rs
@@ -1,10 +1,15 @@
 use crate::*;
 
-// represents `\x -> factor*x + offset`
 #[derive(Clone, PartialEq)]
 struct Linear {
     factor: f64,
     offset: f64
+}
+
+impl Linear {
+    pub fn apply(&self, x: f64) -> f64 {
+        self.factor * x + self.offset
+    }
 }
 
 impl Group for Linear {
@@ -26,6 +31,57 @@ impl Group for Linear {
         Linear {
             factor: 1.0 / self.factor,
             offset: -self.offset/self.factor,
+        }
+    }
+}
+
+struct ConstProp(Option<f64>);
+
+fn is_close(x: f64, y: f64) -> bool { (x - y).abs() <= 1e-10 }
+
+impl Semilattice for ConstProp {
+    type G = Linear;
+
+    fn act(g: &Self::G, s: &Self) -> Self {
+        match s {
+            ConstProp(Some(x)) => ConstProp(Some(g.apply(*x))),
+            ConstProp(None) => ConstProp(None),
+        }
+    }
+
+    fn merge(&mut self, other: Self) -> bool {
+        let ConstProp(Some(o)) = other else { return false };
+
+        match *self {
+            ConstProp(None) => {
+                *self = ConstProp(Some(o));
+                true
+            },
+            ConstProp(Some(x)) => {
+                assert!(is_close(x, o));
+                false
+            },
+        }
+    }
+
+    // TODO this has notable overlap with "merge". Maybe it should just be `from_self_edge(g) -> Self`.
+    // and then "merge" returning false can be used to detect contains_self_edge.
+    fn insert_self_edge(&mut self, g: Self::G) {
+        // a*x + b = x
+        // (a-1)*x = -b
+        // x = b/(1-a)
+        let other = if is_close(g.factor, 1.0) {
+            None
+        } else {
+            Some(g.offset / (1.0 - g.factor - 1.0))
+        };
+        self.merge(ConstProp(other));
+    }
+
+    fn contains_self_edge(&self, g: &Self::G) -> bool {
+        match *self {
+            ConstProp(Some(v)) => g.apply(v) == v,
+            ConstProp(None) => is_close(g.factor, 1.0) && is_close(g.offset, 0.0),
         }
     }
 }
