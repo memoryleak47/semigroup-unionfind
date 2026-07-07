913d9ac2c56dcb87cec029edc5f07bacbd6a85fe
memoryleak47 <memoryleak47@protonmail.ch>
2026-07-07 16:49:36 +0200

    finish first symmatch version

--- rs2/src/symmatch.rs
+++ rs2/src/symmatch.rs
@@ -45,39 +45,62 @@ fn ematch<N: Analysis, M: Matcher<N>>(pat: &Pattern<N::L>, eg: &EGraph<N>) -> Ve
     states.into_iter().map(M::solve).flatten().collect()
 }
 
-fn ematch_impl<'eg, N: Analysis, M: Matcher<N>>(gid: (M::SymG, Id), pat: &Pattern<N::L>, eg: &'eg EGraph<N>, mut state: State<'eg, N, M>) -> Vec<State<'eg, N, M>> {
+fn ematch_impl<'eg, N: Analysis, M: Matcher<N>>((g, id): (M::SymG, Id), pat: &Pattern<N::L>, eg: &'eg EGraph<N>, mut state: State<'eg, N, M>) -> Vec<State<'eg, N, M>> {
     let id_v = alloc_gvar(&mut state);
-    let gid = (M::compose(&gid.0, &M::from_gvar(id_v)), gid.1);
-    state.gs_constraints.insert(id_v, eg.get_leader_semilattice(gid.1));
+    let g = M::compose(&g, &M::from_gvar(id_v));
+    state.gs_constraints.insert(id_v, eg.get_leader_semilattice(id));
 
     // This handles subpatterns without pvars, i.e. terms.
     // Just an optimization, but required for constants/slot-vars,
     // as we need to call "canon" on consts in the pattern anyways.
     if is_term::<N>(pat) {
-        let Some((gg, ii)) = eg.lookup_term(pat) else { return Vec::new() };
-        if gid.1 != ii { return Vec::new() }
-        let cons = M::compose(&M::inverse(&gid.0), &M::from_g(&gg));
+        let Some((g2, id2)) = eg.lookup_term(pat) else { return Vec::new() };
+        if id != id2 { return Vec::new() }
+        let cons = M::compose(&M::inverse(&g), &M::from_g(&g2));
         state.g_constraints.push(cons);
         return vec![state]
     }
 
     match pat {
         Pattern::PVar(v) => {
-            if let Some((g, i)) = state.subst.get(v) {
-                if *i != gid.1 { return Vec::new() }
-                let cons = M::compose(&M::inverse(&gid.0), &g);
+            if let Some((g2, id2)) = state.subst.get(v) {
+                if id != *id2 { return Vec::new() }
+                let cons = M::compose(&M::inverse(&g), &g2);
                 state.g_constraints.push(cons);
             } else {
-                state.subst.insert(*v, gid);
+                state.subst.insert(*v, (g, id));
             }
             vec![state]
         },
-        Pattern::Node(n, childpats) => {
+        Pattern::Node(patnode, childpats) => {
             let mut states = Vec::new();
-            for (gg, mut node) in eg.nodes_of_bare(gid.1) {
-                if !matches::<N>(&node, n) { continue }
-                let expansion = M::expand(&node);
-                todo!()
+            for (gn, mut node) in eg.nodes_of_bare(id) {
+                if !matches::<N>(&node, patnode) { continue }
+                let (pexp, chexp) = M::expand(&node);
+
+                // goal: g*id = Pattern::Node(patnode, childpats)
+                // fact: gn*node = id
+                // -> g*gn*node = Pattern::Node(patnode, childpats)
+
+                // with expansion:
+                // -> g*gn*pexp*node = Pattern::Node(patnode, childpats)
+
+                let cons = M::compose(
+                    &M::compose(&g, &M::from_g(&gn)),
+                    &pexp
+                );
+                state.g_constraints.push(cons);
+
+                let mut substates = vec![state.clone()];
+                let children: Box<[(N::G, Id)]> = N::children_mut(&mut node).into_iter().map(|x| x.clone()).collect();
+                for i in 0..childpats.len() {
+                    for state in std::mem::take(&mut substates) {
+                        let (chg, chi) = &children[i];
+                        let chg = M::compose(&chexp[i], &M::from_g(&chg));
+                        substates.extend(ematch_impl((chg, *chi), &childpats[i], eg, state));
+                    }
+                }
+                states.extend(substates);
             }
             states
         },
