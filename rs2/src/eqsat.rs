cbe6e58fb9f0409f2f180741a104d8266d6001c3
memoryleak47 <memoryleak47@protonmail.ch>
2026-07-29 21:10:29 +0200

    add GeneralRule & Applier

--- rs2/src/eqsat.rs
+++ rs2/src/eqsat.rs
@@ -2,21 +2,48 @@ use crate::*;
 
 pub type Rule<N: Analysis> = (Pattern<N>, Pattern<N>);
 
+pub type GeneralRule<N: Analysis> = (Pattern<N>, Applier<N>);
+pub type Applier<N: Analysis> = Box<dyn Fn((N::G, Id), Subst<N>, &mut EGraph<N>)>;
+
+pub fn pattern_applier<N: Analysis + 'static>(rhs_pat: Pattern<N>) -> Applier<N> {
+    Box::new(move |lhs_eclass, subst, eg| {
+        let rhs_eclass = instantiate(&rhs_pat, eg, &subst);
+        eg.union(lhs_eclass, rhs_eclass);
+    })
+}
+
 // Terms are just patterns that don't contain PVars.
 pub type Term<N: Analysis> = Pattern<N>;
 
 pub fn eqsat<N: Analysis, M: Matcher<N>>(eg: &mut EGraph<N>, rules: &[Rule<N>], n: usize) {
     for _ in 0..n {
         let mut matches = Vec::new();
-        for (lhs, rhs) in rules.iter() {
+        for (lhs, _) in rules.iter() {
             matches.push(ematch::<N, M>(lhs, eg));
         }
 
         for (matches_inner, (lhs, rhs)) in matches.into_iter().zip(rules) {
             for subst in matches_inner {
-                let l = instantiate(lhs, eg, &subst);
-                let r = instantiate(rhs, eg, &subst);
-                eg.uf.union(l, r);
+                let lhs_eclass = instantiate(lhs, eg, &subst);
+                let rhs_eclass = instantiate(rhs, eg, &subst);
+                eg.union(lhs_eclass, rhs_eclass);
+            }
+        }
+        eg.rebuild();
+    }
+}
+
+pub fn general_eqsat<N: Analysis, M: Matcher<N>>(eg: &mut EGraph<N>, rules: &[GeneralRule<N>], n: usize) {
+    for _ in 0..n {
+        let mut matches = Vec::new();
+        for (lhs, _) in rules.iter() {
+            matches.push(ematch::<N, M>(lhs, eg));
+        }
+
+        for (matches_inner, (lhs, applier)) in matches.into_iter().zip(rules) {
+            for subst in matches_inner {
+                let lhs_eclass = instantiate(lhs, eg, &subst);
+                applier(lhs_eclass, subst, eg);
             }
         }
         eg.rebuild();
