61f36fc0058be36d3d3bd8730deafc0b57d2b819
memoryleak47 <memoryleak47@protonmail.ch>
2026-08-05 15:21:36 +0200

    add lib.rs

---
+++ rs2/src/lib.rs
@@ -0,0 +1,27 @@
+use symbol_table::GlobalSymbol as Symbol;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+use std::fmt::Debug;
+
+mod api;
+pub use api::*;
+
+mod fmt;
+
+mod uf;
+pub use uf::*;
+
+mod egraph;
+pub use egraph::*;
+
+mod baseline_ematch;
+pub use baseline_ematch::*;
+
+mod ematch;
+pub use ematch::*;
+
+mod eqsat;
+pub use eqsat::*;
+
+// Examples:
+pub mod cases;
